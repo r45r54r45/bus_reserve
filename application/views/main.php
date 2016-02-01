c285ad1c27880c8bb7da32ae3d0b9ba35acc7354
Woohyun Kim <r45r54r45@naver.com>
2016-02-01 16:07:46 +0900

    dddd

--- application/views/main.php
+++ application/views/main.php
@@ -1,25 +1,35 @@
 <body id="page-top" class="index">
 	<div class="container-fluid">
 		<div class="row">
+			<a href="">
 			<div class="col-xs-12 height-100 thecenter color border-all">
 				<span class="extra-font">FRESHMAN YONSEI</span>
 			</div>
+		</a>
 		</div>
 		<div class="row">
+						<a href="">
 			<div class="col-xs-6 height-300 thecenter color border-all">
 				<span class="big-font">익명으로<br>물어보장!</span>
 			</div>
+		</a>
+					<a href="">
 			<div class="col-xs-6 height-300 thecenter color border-all">
 				<span class="big-font">셔틀예약<br>쉽게하장!</span>
 			</div>
+		</a>
 		</div>
 		<div class="row">
+						<a href="">
 			<div class="col-xs-6 height-300 thecenter color border-all">
 				<span class="big-font">오늘 점심은<br>모먹지?</span>
 			</div>
+		</a>
+				<a href="">
 			<div class="col-xs-6 height-300 thecenter color border-all">
 				<span class="big-font">^오^</span>
 			</div>
+		</a>
 		</div>
 	</div>
 </body>
