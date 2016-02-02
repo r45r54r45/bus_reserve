670c01f7ec931340df3e545e2f4c53db8bf53036
Woohyun Kim <r45r54r45@naver.com>
2016-02-02 21:54:47 +0900

    ㅇㅇ

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,11 +1,20 @@
 <body>
 <form action="/data/food_add" method="post">
+  <div>
+    날짜
 <input name="date" type="date">
+</div>
+<div>
+  1긱
 <textarea name="data1">
 
 </textarea>
+</div>
+<div>
+  2긱
 <textarea name="data2">
 
 </textarea>
-<button type="submit"></form>
+</div>
+<button type="submit" value="전송"></form>
 </body>
