9b67afef1ae3a14a3001271b9d7b811fada3b764
Woohyun Kim <r45r54r45@naver.com>
2016-02-02 19:30:05 +0900

    hh

--- application/controllers/Data.php
+++ application/controllers/Data.php
@@ -17,5 +17,24 @@ class Data extends CI_Controller {
 		$options->text = $_GET['data'];
 		$rest->send($options);
 	}
-
+	public function food()
+	{
+		$f1=array("","");
+		$f2=array("뚝)뼈없는감자탕
+[돈육:미국산,돈잡뼈:국
+내산]
+쌀밥
+새우완자전
+진미채무침
+깍두기","");
+		$f3=array("새우튀김우동
+김가루양념밥
+순대찜
+단무지
+배추김치","");
+		$one=array($f1,$f2,$f3);
+		$two=array($f1,$f2,$f3);
+		$arr = array ($one,$two);
+		echo json_encode($arr);
+	}
 }
--- application/views/main.php
+++ application/views/main.php
@@ -26,22 +26,48 @@
 								<div class="row">
 									<div class="col-xs-12 col-sm-4">
 										<div class="row">
-											<div class="col-xs-12">
+											<div class="col-xs-12 grey">
 												아침
 											</div>
-											<div class="col-xs-6">
-												메뉴1
+											<div  class="col-xs-6">
+												<pre id="one_0_1">
+												</pre>
 											</div>
-											<div class="col-xs-6">
-												메뉴2
+											<div  class="col-xs-6">
+												<pre id="one_0_2">
+												</pre>
 											</div>
 										</div>
 									</div>
 									<div class="col-xs-12 col-sm-4">
-										점심
+										<div class="row">
+											<div class="col-xs-12 grey">
+												점심
+											</div>
+											<div  class="col-xs-6">
+												<pre id="one_1_1">
+												</pre>
+											</div>
+											<div  class="col-xs-6">
+												<pre id="one_1_2">
+												</pre>
+											</div>
+										</div>
 									</div>
 									<div class="col-xs-12 col-sm-4">
-										저녁
+										<div class="row">
+											<div class="col-xs-12 grey">
+												저녁
+											</div>
+											<div class="col-xs-6">
+												<pre id="one_2_1">
+												</pre>
+											</div>
+											<div class="col-xs-6">
+												<pre id="one_2_2">
+												</pre>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -56,101 +82,161 @@
 					</div>
 					<div class="col-xs-12">
 						<div class="collapse "  id="2dorm">
-							<div class="well" >
-								fff
+							<div class="well color-black" >
+								<div class="row">
+									<div class="col-xs-12 col-sm-4">
+										<div class="row">
+											<div class="col-xs-12 grey">
+												아침
+											</div>
+											<div class="col-xs-6">
+												<pre id="two_0_1">
+												</pre>
+											</div>
+											<div class="col-xs-6">
+												<pre id="two_0_2">
+												</pre>
+											</div>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-4">
+										<div class="row">
+											<div class="col-xs-12 grey">
+												점심
+											</div>
+											<div class="col-xs-6">
+												<pre id="two_1_1">
+												</pre>
+											</div>
+											<div class="col-xs-6">
+												<pre id="two_1_2">
+												</pre>
+											</div>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-4">
+										<div class="row">
+											<div class="col-xs-12 grey">
+												저녁
+											</div>
+											<div class="col-xs-6">
+												<pre id="two_2_1" >
+												</pre>
+											</div>
+											<div class="col-xs-6">
+												<pre id="two_2_2" >
+												</pre>
+											</div>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-xs-12" style="margin-top:20px;">
-						<div class="btn-group btn-group-justified " role="group">
-							<div class="btn-group" role="group">
-								<button type="button" class="btn btn-default" data-toggle="collapse" data-target="#raon" aria-expanded="false" aria-controls="collapse">라온샘</button>
-							</div>
-						</div>
-					</div>
-					<div class="col-xs-12">
-						<div class="collapse "  id="raon">
-							<div class="well" >
-								fff
-							</div>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-			<!-- 학식 메뉴 끝 -->
-			<!-- 셔틀 시간 시작-->
-			<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
-				<div>
-					<div class="big-font">
-						해경가서 술이나?<br> <span style="font-size:12px;">송도 안 셔틀버스</span>
-					</div>
-					<div class="normal-font" style="margin-top:30px;">
-						<span>해경으로 가요~!</span>
-						<span id="h_s"></span>
-					</div>
-					<div class="normal-font" style="margin-top:5px;">
-						<span>해경에서 긱사로~!</span>
-						<span id="h_w"></span>
-					</div>
-					<div class="normal-font" style="margin-top:5px;">
-						<span>영화보러 동춘으로~?</span>
-						<span id="d_s"></span>
-					</div>
-					<div class="normal-font" style="margin-top:5px;">
-						<span>동춘에서 긱사로~!</span>
-						<span id="d_w"></span>
-					</div>
-					<div class="normal-font" style="margin-top:30px;">
-						<span>실시간이니 걱정 ㄴㄴ해</span>
-					</div>
-				</div>
-			</div>
-			<!-- 셔틀 시간 끝-->
-			<!-- 6724 시간 시작-->
-			<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
-				<div>
-					<div class="big-font">
-						<span class="normal-font">버스앱보다 정확한</span><br> <span class="big-font">M6724 시간 볼래?</span><br>
-						<span class="small-font" style="margin-top:10px;">&plusmn;3분은 예의상 알지?</span>
-					</div>
-				</div>
-			</div>
-			<!-- 6724 시간 끝-->
-			<!-- 익명 질문 시작 -->
-			<a href="">
-				<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
-					<span class="big-font">익명으로<br>물어보장!</span>
-				</div>
-			</a>
-			<!-- 익명 질문 끝 -->
-			<!-- 셔틀예약 시작 -->
-			<a href="">
-				<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
-					<span class="big-font">셔틀예약<br>편하게하장!</span>
-				</div>
-			</a>
-			<!-- 셔틀예약 끝 -->
-			<div class="hidden-xs col-sm-6 height-300 thecenter color border-all">
-				<span class="big-font">^오^</span>
-			</div>
-			<!-- 의견 받기 시작 -->
-			<div id="help"  class="col-xs-12  height-100 thecenter color border-all">
-				<div>
-				<div style="margin-top:10px;color:white;" class="normal-font">디자인 구린거 알아요..쳇<br>도와주려면 연락 좀.. <a style="color:white;" href="tel:01071097327">전화걸기</a></div>
-				<div style="display:table;margin:10px 0;" class="input-group">
-					<input id="helpval" maxlength="45" class="form-control" placeholder="필요한거,조언할거는 여기에">
-					<span class="input-group-btn">
-						<button class="btn btn-default" type="button" onclick="help()">보내기</button>
-					</span>
+					<!-- <div class="col-xs-12" style="margin-top:20px;">
+					<div class="btn-group btn-group-justified " role="group">
+					<div class="btn-group" role="group">
+					<button type="button" class="btn btn-default" data-toggle="collapse" data-target="#raon" aria-expanded="false" aria-controls="collapse">라온샘</button>
 				</div>
 			</div>
 		</div>
-		<!-- 의견받기 끝 -->
+		<div class="col-xs-12">
+		<div class="collapse "  id="raon">
+		<div class="well" >
+		fff
+	</div>
+</div>
+</div> -->
+<div class="clearfix"></div>
+</div>
+</div>
+<!-- 학식 메뉴 끝 -->
+<!-- 셔틀 시간 시작-->
+<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
+	<div>
+		<div class="big-font">
+			해경가서 술이나?<br> <span style="font-size:12px;">송도 안 셔틀버스</span>
+		</div>
+		<div class="normal-font" style="margin-top:30px;">
+			<span>해경으로 가요~!</span>
+			<span id="h_s"></span>
+		</div>
+		<div class="normal-font" style="margin-top:5px;">
+			<span>해경에서 긱사로~!</span>
+			<span id="h_w"></span>
+		</div>
+		<div class="normal-font" style="margin-top:5px;">
+			<span>영화보러 동춘으로~?</span>
+			<span id="d_s"></span>
+		</div>
+		<div class="normal-font" style="margin-top:5px;">
+			<span>동춘에서 긱사로~!</span>
+			<span id="d_w"></span>
+		</div>
+		<div class="normal-font" style="margin-top:30px;">
+			<span>실시간이니 걱정 ㄴㄴ해</span>
 		</div>
 	</div>
+</div>
+<!-- 셔틀 시간 끝-->
+<!-- 6724 시간 시작-->
+<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
+	<div>
+		<div class="big-font">
+			<span class="normal-font">버스앱보다 정확한</span><br> <span class="big-font">M6724 시간 볼래?</span><br>
+			<span class="small-font" style="margin-top:10px;">&plusmn;3분은 예의상 알지?</span>
+		</div>
+	</div>
+</div>
+<!-- 6724 시간 끝-->
+<!-- 익명 질문 시작 -->
+<a href="">
+	<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
+		<span class="big-font">익명으로<br>물어보장!</span>
+	</div>
+</a>
+<!-- 익명 질문 끝 -->
+<!-- 셔틀예약 시작 -->
+<a href="">
+	<div class="col-xs-12 col-sm-6 height-300 thecenter color border-all">
+		<span class="big-font">셔틀예약<br>편하게하장!</span>
+	</div>
+</a>
+<!-- 셔틀예약 끝 -->
+<div class="hidden-xs col-sm-6 height-300 thecenter color border-all">
+	<span class="big-font">^오^</span>
+</div>
+<!-- 의견 받기 시작 -->
+<div id="help"  class="col-xs-12  height-100 thecenter color border-all">
+	<div>
+		<div style="margin-top:10px;color:white;" class="normal-font">디자인 구린거 알아요..쳇<br>도와주려면 연락 좀.. <a style="color:white;" href="tel:01071097327">전화걸기</a></div>
+		<div style="display:table;margin:10px 0;" class="input-group">
+			<input id="helpval" maxlength="45" class="form-control" placeholder="필요한거,조언할거는 여기에">
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="button" onclick="help()">보내기</button>
+			</span>
+		</div>
+	</div>
+</div>
+<!-- 의견받기 끝 -->
+</div>
+</div>
 </body>
 <script>
+function getFood(){
+	$.get("/data/food", function( data ) {
+		var one=data[0];
+		var two=data[1];
+		for (var i = 0; i < one.length; i++) {
+			$("#one_"+i+"_1").text(one[i][0]); //음식 1
+			$("#one_"+i+"_2").text(one[i][1]); //음식 2
+		}
+		for (var i = 0; i < two.length; i++) {
+			$("#two_"+i+"_1").text(two[i][0]); //음식 1
+			$("#two_"+i+"_2").text(two[i][1]); //음식 2
+		}
+	}, "json" );
+}
 function help(){
 	var data=$("#helpval").val();
 	if(data==""){
@@ -158,7 +244,7 @@ function help(){
 		return;
 	}
 	$.get("/data/help?data="+data);
-		$("#help").contents().remove();
+	$("#help").contents().remove();
 	$("#help").append('<span class="big-font">고맙당</span>');
 }
 // 셔틀 시간정보
@@ -174,6 +260,7 @@ for (var i = 0; i < d_time_s.length; i++) {
 }
 $(window).on("load",function(){
 	songdo_shuttle();
+	getFood();
 });
 function findPos(arr,data){
 	for (var i = 0; i < arr.length; i++) {
