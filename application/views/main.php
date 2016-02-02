18049f8d61950362753563e4a360d23f4804a158
Woohyun Kim <r45r54r45@naver.com>
2016-02-02 18:46:32 +0900

    jj

--- application/views/main.php
+++ application/views/main.php
@@ -133,7 +133,7 @@
 			<!-- 셔틀예약 끝 -->
 			<div id="help" style="position:fixed;bottom:0;" class="col-xs-12 col-sm-6 height-100 thecenter color border-all">
 				<div>
-				<div style="margin-top:10px;color:white;" class="normal-font">디자인 구린거 알아요..쳇<br>도와주려면 연락 좀.. <a style="color:white;" href="tel:01071097327">전화걸기</a></div>
+				<div style="margin-top:10px;color:white;" class="small-font">디자인 구린거 알아요..쳇<br>도와주려면 연락 좀.. <a style="color:white;" href="tel:01071097327">전화걸기</a></div>
 				<div style="display:table;margin:10px 0;" class="input-group">
 					<input id="helpval" maxlength="45" class="form-control" placeholder="필요한거,조언할거는 여기에">
 					<span class="input-group-btn">
