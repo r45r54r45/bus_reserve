34b0df88b511bedbfdc519d9c4adf6684eeddcf6
Woohyun Kim <r45r54r45@naver.com>
2016-02-05 19:25:01 +0900

    ㅊㅊ

--- application/views/header.php
+++ application/views/header.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
-    <title>테스트</title>
+    <title>송도라이프</title>
     <script src="https://code.jquery.com/jquery-2.2.0.min.js" crossorigin="anonymous" ></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
