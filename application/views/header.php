f3a7d6d6c859e33906bff9c9eca8e7d3eb3a461e
Woohyun Kim <r45r54r45@naver.com>
2016-02-07 23:08:30 +0900

    ㅇㅇ

--- application/views/header.php
+++ application/views/header.php
@@ -9,16 +9,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
     <title>송도라이프</title>
-    <script src="https://code.jquery.com/jquery-2.2.0.min.js" crossorigin="anonymous" ></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
-    <script src="/src/js/jquery.mobile.custom.min.js"></script>
-    <script src="/src/js/common.js"></script>
 
     <!-- CSS -->
     <link rel="stylesheet"  href="/src/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/css/bootstrap-select.min.css">
     <link rel="stylesheet" href="/src/css/common.css">
+    <link rel="stylesheet" href="/src/css/dropzone.css" >
+
+
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" crossorigin="anonymous" ></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
+    <script src="/src/js/jquery.mobile.custom.min.js"></script>
+    <script src="/src/js/common.js"></script>
+    <script src="/src/js/dropzone.js" ></script>
+
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- application/views/print_vault.php
+++ application/views/print_vault.php
@@ -6,6 +6,10 @@ function submit_form(){
   }
   $("#form").submit();
 }
+Dropzone.autoDiscover = false;
+$(function(){
+  var myDropzone1 = new Dropzone("#upload", { url:'/prints/file' , paramName: "userfile",addRemoveLinks:true,dictRemoveFile:"파일 삭제"});
+});
 </script>
 <body>
   <div class="container-fluid alert alert-success" style="margin-top:20px;">
@@ -19,14 +23,16 @@ function submit_form(){
           <div class="col-xs-12 center side-padding-20 form-group" style="padding-top:10px;">
             <div class="full-width input-group">
               <form id="form3" method="post" action="/prints/file" enctype="multipart/form-data">
-              <input name="userfile" class="form-control" type="file" >
+              <div id="upload" class="dropzone">
+                  <div class="dz-default dz-message"><span>프로필 사진은 한 개만 업로드 할 수 있습니다<br>( 클릭 또는 파일을 드래그 해주세요! )</span></div>
+                </div>
             </form>
             </div>
           </div>
           <div class="col-xs-12" role="alert">
             <span class="glyphicon glyphicon-info-sign" style="margin-left:10px;vertical-align: middle"></span>
             <span class="small-font" style="font-size:12px;">
-              최대 15mb까지 업로드 가능합니다.
+              한 파일당 최대 15mb까지 업로드 가능합니다.
             </span>
           </div>
           <div class="col-xs-12 side-padding-20">
