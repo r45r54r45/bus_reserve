e794c8060f98d45893872201e184b9a20b36e021
Woohyun Kim <r45r54r45@naver.com>
2016-02-08 15:36:36 +0900

    ddd

--- application/views/test.php
+++ application/views/test.php
@@ -1,5 +1,4 @@
-<form action="/prints/file" method="post">
+<form action="/prints/file" method="post" enctype="multipart/form-data">
 <input type="file" name="userfile">
 <button type="submit">ddd</button>
 </form>
-  
