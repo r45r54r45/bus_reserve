a259bf585994396eafb812fb2b4160930dcfef20
Woohyun Kim <r45r54r45@naver.com>
2016-02-10 00:10:22 +0900

    mnnn

--- application/controllers/Prints.php
+++ application/controllers/Prints.php
@@ -179,7 +179,7 @@ class Prints extends CI_Controller {
 			echo '
 			<tr>
 			<td>'.$f_name.'</td>
-			<td><a href="prints/download?num='.$f_idx.'"><span class="glyphicon glyphicon-download"></span></a></td>
+			<td><a href="prints/download2?num='.$f_idx.'"><span class="glyphicon glyphicon-download"></span></a></td>
 			</tr>
 			';
 		}
