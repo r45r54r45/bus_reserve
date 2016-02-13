9eaf0a158e6afcc434e22b0e07584a0a8bff44e1
Woohyun Kim <r45r54r45@naver.com>
2016-02-14 02:47:46 +0900

    ddd

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -8,7 +8,7 @@ class App extends CI_Controller {
 		$json = file_get_contents('php://input');
 		$obj = json_decode($json);
 		$this->load->model("app_model");
-		$this->app_model->regit($obj['regId']);
+		$this->app_model->regit($obj->{"regId"});
 	}
 	public function gcmSender(){
 		$this->load->view('app/gcmSender_func');
