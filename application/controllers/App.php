ebcee13fb92a37cc1eb4c9cc8e89102a179477ba
Woohyun Kim <r45r54r45@naver.com>
2016-02-14 02:33:43 +0900

    ddd

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -5,7 +5,10 @@ class App extends CI_Controller {
 
 	public function register()
 	{
-		$this->load->view('app/register');
+		$json = file_get_contents('php://input');
+		$obj = json_decode($json);
+		$this->load->model("app_model");
+		$this->app_model->regit($obj['regId']);
 	}
 	public function gcmSender(){
 		$this->load->view('app/gcmSender_func');
---
+++ application/models/App_model.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class App_model extends CI_Model{
+	public function __construct(){
+		$this->load->database();
+	}
+	public function regit($data){
+		return $this->db->query("insert into regit_list values ('$data')");
+	}
+
+}
