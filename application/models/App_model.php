89954511affe2582ad3ec51f132298396c8533f7
Woohyun Kim <r45r54r45@naver.com>
2016-02-14 03:48:01 +0900

    mnm

--- application/models/App_model.php
+++ application/models/App_model.php
@@ -6,7 +6,7 @@ class App_model extends CI_Model{
 		$this->load->database();
 	}
 	public function regit($data){
-		return $this->db->query("insert into regit_list values ('$data') where not exists (select * from regit_list where regit='$data')");
+		return $this->db->query("replace into regit_list values ('$data')");
 	}
 	public function getRegits(){
 		return $this->db->query("select * from regit_list");
