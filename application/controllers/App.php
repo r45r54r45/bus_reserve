64c8d797c20c00bc0a4e4ed7bc3fed46dd36b459
Woohyun Kim <r45r54r45@naver.com>
2016-02-14 03:05:18 +0900

    ccc

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -12,6 +12,31 @@ class App extends CI_Controller {
 		$this->app_model->regit($_GET["regId"]);
 	}
 	public function gcmSender(){
-		$this->load->view('app/gcmSender_func');
+		$this->load->model("app_model");
+		$regits=$this->app_model->getRegits();
+		$headers = array(
+		 'Content-Type:application/json',
+		 'Authorization:key=AIzaSyACU6iZvyBcFURa6w_BYRIqVzxqdP8_sos'
+		);
+
+		$arr   = array();
+		$arr['data'] = array();
+		$arr['data']['data']="fuck fuck";
+		$arr['data']['type']="text";
+		$arr['data']['command']="show";
+		$arr['registration_ids'] = array();
+		$arr['registration_ids'] = $regits;
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL,    'https://android.googleapis.com/gcm/send');
+		curl_setopt($ch, CURLOPT_HTTPHEADER,  $headers);
+		curl_setopt($ch, CURLOPT_POST,    true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arr));
+		$response = curl_exec($ch);
+		echo $response;
+		curl_close($ch);
 	}
 }
