6b0b2a3134759596df9d18552aa93ccd50e252cf
Woohyun Kim <r45r54r45@naver.com>
2016-02-14 03:02:05 +0900

    nn

--- application/views/app/gcmSender_func.php
+++ application/views/app/gcmSender_func.php
@@ -10,7 +10,7 @@ $arr['data']['data']="fuck fuck";
 $arr['data']['type']="text";
 $arr['data']['command']="show";
 $arr['registration_ids'] = array();
-$arr['registration_ids'] = $data['regits'];
+$arr['registration_ids'] = $regits;
 
 $ch = curl_init();
 
