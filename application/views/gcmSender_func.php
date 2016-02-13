1a24d6f202edf8b92f537db328dc40dc86d55a18
Woohyun Kim <r45r54r45@naver.com>
2016-02-14 01:22:33 +0900

    ddd

--- application/views/gcmSender_func.php
+++ application/views/gcmSender_func.php
@@ -5,8 +5,9 @@ $headers = array(
 );
 
 $arr   = array();
-$arr['data'] = array();
-$arr['data']['msg'] = "{data:gcm으로 보낼 메시지를 쓰면 된다.}";
+$arr['data'] = "fuck fuck";
+$arr['type']="text";
+$arr['command']="show";
 $arr['registration_ids'] = array();
 $arr['registration_ids'][0] = "APA91bGIv534n9Rb-U8MMKfdnuDsyJ2S7aqgAjbwlUQI4fvxUsR6hYfLBVYcdx9cPxRU31RhA37KxbaSsI0eYCOU1BOIVRKnrOluR_neZ95euaAEx1zwi30gdpy3VvJ2R9xRvE1xV3gq";
 
