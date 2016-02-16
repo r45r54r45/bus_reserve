c99fab51076e965baa0d5a0a4f505b1e0d87ef7c
Woohyun Kim <r45r54r45@naver.com>
2016-02-16 21:00:19 +0900

    ddd

--- application/models/App_model.php
+++ application/models/App_model.php
@@ -6,13 +6,13 @@ class App_model extends CI_Model{
 		$this->load->database();
 	}
 	public function regit($data){
-		return $this->db->query("replace into regit_list values ('$data')");
+		return $this->db->query("replace into regit_list (gcm) values ('$data')");
 	}
 	public function getRegits(){
 		return $this->db->query("select * from regit_list");
 	}
 	public function isRegister($gcm,$token){
-		$result=$this->db->query("select count(*) from regit_list where regit='$gcm' and facebook_token='$token'");
+		$result=$this->db->query("select count(*) from regit_list where gcm='$gcm' and facebook_token='$token'");
 		$cnt=0;
 		foreach ($result->result_array as $row) {
 			$cnt++;
