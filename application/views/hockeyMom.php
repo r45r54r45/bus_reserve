49930fcff08f1b84e78893cfb4481ca13252b08b
Woohyun Kim <r45r54r45@naver.com>
2016-02-26 16:24:15 +0900

    ddd

---
+++ application/views/hockeyMom.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+  <title>입시 컨설팅 플랫폼</title>
+
+  <link rel="stylesheet"  href="/src/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-2.2.0.min.js" crossorigin="anonymous" ></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <img src="http://www.muaac.org/front/data/cheditor4/1403/4e5d18ded39d245db7912b8f0fe84861_1393901932.jpg" class="img img-responsive">
+  </div>
+</body>
+</html>
