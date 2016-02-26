e09676e5fe18af46f8ee4005f3c6106d15f691ab
Woohyun Kim <r45r54r45@naver.com>
2016-02-26 16:37:10 +0900

    ddd

--- application/views/hockeyMom.php
+++ application/views/hockeyMom.php
@@ -12,10 +12,87 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
 </head>
 <body>
+  <style>
+    .countdown{
+      display: inline-block;
+      text-align: center;
+    }
+    .time{
+      font-size:60px;
+      font-weight: 400;
+    }
+    .desc{
+
+    }
+  </style>
+  <script>
+  $(function(){
+    setInterval(function(){
+      // var time=
+      // $()
+    },1000);
+  });
+  </script>
   <div class="container" style="position:relative;">
     <div style="position:absolute; left:0; top:0;">
     <img src="http://www.muaac.org/front/data/cheditor4/1403/4e5d18ded39d245db7912b8f0fe84861_1393901932.jpg" class="img img-responsive">
   </div>
+  <div class="row" style="">
+    <div class="col-xs-12">
+      <span style="font-weight:600; font-size:40px;">사이트 제목</span>
+    </div>
+    <div class="col-xs-12">
+      <span style="font-weight:500; font-size:30px;">사이트 부제목</span>
+    </div>
+  </div>
+    <div class="row" style="">
+      <div class="countdown" style="width:10%">
+      </div>
+      <div class="countdown" style="width:20%">
+        <div class="time" id="day">
+        </div>
+        <div class="desc">
+        </div>
+      </div>
+      <div class="countdown" style="width:20%">
+        <div class="time" id="hour">
+        </div>
+        <div class="desc">
+        </div>
+      </div>
+      <div class="countdown" style="width:20%">
+        <div class="time" id="minute">
+        </div>
+        <div class="desc">
+        </div>
+      </div>
+      <div class="countdown" style="width:20%">
+        <div class="time" id="seconds">
+        </div>
+        <div class="desc">
+        </div>
+      </div>
+      <div class="countdown" style="width:10%">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12" style="text-align:center;">
+      <span style="font-weight:400; font-size:20px;">소식을 들으시려면 해주셈</span>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12" style="text-align:center;">
+      <span style="font-weight:400; font-size:20px;">소식을 들으시려면 해주셈</span>
+    </div>
+    <div class="col-xs-12" style="text-align:center;">
+      <input>
+    </div>
+    </div>
+    <div class="row">
+      <div
+    </div>
+
   </div>
 </body>
 </html>
