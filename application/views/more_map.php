dd46b82b930246efb9a0ec362eb09e4547369ced
Woohyun Kim <r45r54r45@naver.com>
2016-02-29 00:51:43 +0900

    ccc

--- application/views/more_map.php
+++ application/views/more_map.php
@@ -1,24 +1,24 @@
 <div id="grid">
 
            <div class="grid-element">
-               <span>Label</span>
+               <span id="g11">맛불작전</span>
            </div>
 
            <div class="grid-element">
-               <span>Label</span>
+               <span id="g12">Label</span>
            </div>
 
            <div class="grid-element">
-               <span>Label</span>
+               <span id="g13">Label</span>
            </div>
 
 
            <div class="grid-element">
-               <span>Label</span>
+               <span id="g21">Label</span>
            </div>
 
            <div class="grid-element">
-               <span>Label</span>
+               <span id="g22">Label</span>
            </div>
 
            <div class="grid-element">
@@ -47,12 +47,14 @@
            }
            #grid{
                width: 100%;
-               padding:5px;
+               padding:2vw 2vw;
            }
            .grid-element{
-               width:33%;
-
+               width:32vw;
+               height:32vw;
                float:left;
+               text-align: center;
+               line-height: 32vw;
            }
            #grid>div{
              border:1px solid black;
