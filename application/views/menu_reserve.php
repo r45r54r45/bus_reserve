4ef26c4324c7a86f13db8981e76b7f588e47c5ef
Woohyun Kim <r45r54r45@naver.com>
2016-02-28 15:55:03 +0900

    ㅊㅊ

--- application/views/menu_reserve.php
+++ application/views/menu_reserve.php
@@ -3,15 +3,13 @@
     <div id="home" class="btn-group" data-toggle="buttons">
       <label class="btn btn-primary active" style="border-bottom-left-radius: 16px;
       border-top-left-radius: 16px;" id="option1">
-      <input type="radio" name="options"  autocomplete="off"> 1학사
-    </label>
-    <label class="btn btn-primary" style="" id="option2">
-      <input type="radio" name="options"  autocomplete="off"> 2학사
+      <input type="radio" name="options"  autocomplete="off"> 세미나실
     </label>
     <label class="btn btn-primary" style="border-bottom-right-radius: 16px;
-    border-top-right-radius: 16px;" id="option3">
-    <input type="radio" name="options"  autocomplete="off"> 언기도
-  </label>
+    border-top-right-radius: 16px;" id="option2">
+      <input type="radio" name="options"  autocomplete="off"> 열람실
+    </label>
+
 </div>
 </div>
 </div>
@@ -24,14 +22,13 @@
 </iframe>
 
 <script>
-var type=0;
 $(function(){
   $("#frame").css("height",$(window).height()-50+"px");
   $("#option1").on("click",function(){
-    $("#frame").attr("src","http://naver.com");
+    $("#frame").attr("src","https://yicdorm.yonsei.ac.kr/room.asp");
   });
   $("#option2").on("click",function(){
-    $("#frame").attr("src","http://daum.com");
+    $("#frame").attr("src","https://yicdorm.yonsei.ac.kr/study.asp");
   });
 });
 </script>
