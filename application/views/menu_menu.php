66dd60c5bdb5cd820e9d70395f708a90fc1e8eb6
Woohyun Kim <r45r54r45@naver.com>
2016-02-28 15:03:25 +0900

    cc

--- application/views/menu_menu.php
+++ application/views/menu_menu.php
@@ -1,1 +1,26 @@
-menu
+<div class="container-fluid">
+  <div class="row center-align" id="home-top-button">
+    <div id="home" class="btn-group" data-toggle="buttons">
+      <label class="btn btn-primary active" style="border-bottom-left-radius: 16px;
+      border-top-left-radius: 16px;" id="option1">
+      <input type="radio" name="options"  autocomplete="off"> 1학사
+    </label>
+    <label class="btn btn-primary" style="" id="option2">
+      <input type="radio" name="options"  autocomplete="off"> 2학사
+    </label>
+    <label class="btn btn-primary" style="border-bottom-right-radius: 16px;
+    border-top-right-radius: 16px;" id="option3">
+    <input type="radio" name="options"  autocomplete="off"> 언기도
+  </label>
+</div>
+</div>
+<div class="row">
+  <div class="col-xs-12">
+    <div class="card_holder">
+      <div class="card" style="height:100px;">
+      </div>
+    </div>
+  </div>
+</div>
+<!-- end of container -->
+</div>
