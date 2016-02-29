a391f88612ef7cc7441a6081c43388c474475a1f
Woohyun Kim <r45r54r45@naver.com>
2016-02-29 19:29:23 +0900

    ㅠ

--- application/views/test.php
+++ application/views/test.php
@@ -1,9 +1,10 @@
 <?
+header('Access-Control-Allow-Origin:*');
 header('Content-Type: application/json');
+
 $url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?ServiceKey=JwH8P3QCOaTv%2FYttZkCDASHiJZblEJAk8X2i6nICtptOT8O55JvqRPG7nluVp6QEVQu7sy51aHk%2FUgm2b0Ncyg%3D%3D&cityCode=23&routeId=ICB165000381&nodeId=ICB165000725"; //6724
 $response = file_get_contents($url);
 $object = simplexml_load_string($response);
-$json = json_encode($object);
 $count6724=$object->body->items->item->arrprevstationcnt;
 $time6724= $object->body->items->item->arrtime;
 
