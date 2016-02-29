da54ff726d5c88787f9f40a8a9823e150a53b88f
Woohyun Kim <r45r54r45@naver.com>
2016-02-29 18:12:52 +0900

    ddd

--- application/views/test.php
+++ application/views/test.php
@@ -3,5 +3,6 @@
 $url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?ServiceKey=JwH8P3QCOaTv%2FYttZkCDASHiJZblEJAk8X2i6nICtptOT8O55JvqRPG7nluVp6QEVQu7sy51aHk%2FUgm2b0Ncyg%3D%3D&cityCode=23&routeId=ICB165000381&nodeId=ICB165000725";
 
 $response = file_get_contents($url);
+echo $response;
 $object = simplexml_load_string($response);
 echo $object;
