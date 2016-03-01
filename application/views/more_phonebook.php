3b6d7d08739c122b746c806c6833022ba682380d
Jaeha Lee <jhl.webproduction@gmail.com>
2016-03-02 02:47:01 +0900

    ㅁㄴㅇ

---
+++ application/views/more_phonebook.php
@@ -0,0 +1,137 @@
+<div class="container-fluid">
+  <div class="row" style="margin-top:10px;">
+    <div class="col-xs-6" style="padding:0 5px;float:left;">
+      <!-- 제1기숙사 행정실 -->
+      <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+        <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+          <div><span style="font-size: 12px;">제1기숙사 행정실</span></div>
+          <div  style="margin-top: -5px;"></div>
+        </div>
+        <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+          <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+          </div>
+          <div style="float: right;line-height: 40px;">
+            <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+          </div>
+        </div>
+      </div>
+      <!-- 제2기숙사 행정실 -->
+      <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+        <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+          <div><span style="font-size: 12px;">제2기숙사 행정실</span></div>
+          <div  style="margin-top: -5px;"></div>
+        </div>
+        <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+          <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+          </div>
+          <div style="float: right;line-height: 40px;">
+            <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+          </div>
+        </div>
+      </div>
+      <!-- 언더우드기념도서관-->
+      <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+        <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+          <div><span style="font-size: 12px;">언더우드기념도서관</span></div>
+          <div  style="margin-top: -5px;"></div>
+        </div>
+        <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+          <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+          </div>
+          <div style="float: right;line-height: 40px;">
+            <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+          </div>
+        </div>
+      </div>
+      <!-- 대학교회-->
+      <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+        <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+          <div><span style="font-size: 12px;">대학교회</span></div>
+          <div  style="margin-top: -5px;"></div>
+        </div>
+        <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+          <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+          </div>
+          <div style="float: right;line-height: 40px;">
+            <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+          </div>
+        </div>
+      </div>
+      <!-- 건강센터-->
+      <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+        <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+          <div><span style="font-size: 12px;">건강센터</span></div>
+          <div  style="margin-top: -5px;"></div>
+        </div>
+        <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+          <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+          </div>
+          <div style="float: right;line-height: 40px;">
+            <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
+
+
+  <div class="col-xs-6" style="padding:0 5px;float:left;">
+    <!-- 제1기숙사 관리실-->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">제1기숙사 관리실</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+    <!-- 제2기숙사 관리실 -->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">제2기숙사 관리실</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+    <!-- UIC 사무실 -->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">UIC 사무실</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+    <!-- 교목실 -->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">교목실</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
