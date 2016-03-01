bba0ccc8c43a65ff6756353d1f53b0a5e119db2a
Jaeha Lee <jhl.webproduction@gmail.com>
2016-03-02 02:59:47 +0900

    ㅁㄴㅇ

--- application/views/more_phonebook.php
+++ application/views/more_phonebook.php
@@ -1,5 +1,14 @@
 <div class="container-fluid">
+  <div class="row center-align" id="home-top-button">
+    <div id="home" class="btn-group" data-toggle="buttons">
+      <label class="btn btn-primary active" style="border-bottom-left-radius: 16px;
+      border-top-left-radius: 16px;" id="option1">
+      <input type="radio" name="options"  autocomplete="off"> 국제캠퍼스
+    </label>
+    </div>
+  </div>
   <div class="row" style="margin-top:10px;">
+    <!--first col-->
     <div class="col-xs-6" style="padding:0 5px;float:left;">
       <!-- 제1기숙사 행정실 -->
       <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
@@ -43,20 +52,6 @@
           </div>
         </div>
       </div>
-      <!-- 대학교회-->
-      <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
-        <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
-          <div><span style="font-size: 12px;">대학교회</span></div>
-          <div  style="margin-top: -5px;"></div>
-        </div>
-        <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
-          <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
-          </div>
-          <div style="float: right;line-height: 40px;">
-            <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
-          </div>
-        </div>
-      </div>
       <!-- 건강센터-->
       <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
         <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
@@ -72,10 +67,7 @@
         </div>
       </div>
   </div>
-
-
-
-
+  <!-- second col-->
   <div class="col-xs-6" style="padding:0 5px;float:left;">
     <!-- 제1기숙사 관리실-->
     <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
@@ -134,4 +126,108 @@
       </div>
     </div>
   </div>
+</div>
+<div class="row center-align" id="home-top-button">
+  <div id="home" class="btn-group" data-toggle="buttons">
+    <label class="btn btn-primary active" style="border-bottom-left-radius: 16px;
+    border-top-left-radius: 16px;" id="option1">
+    <input type="radio" name="options"  autocomplete="off"> 신촌캠퍼스
+  </label>
+  </div>
+</div>
+<!--second row-->
+<div class="row" style="margin-top:10px;">
+  <!--first col-->
+  <div class="col-xs-6" style="padding:0 5px;float:left;">
+    <!-- 교무처 -->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">교무처</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+    <!-- 입학처 -->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">입학처</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+    <!-- 학생복지처-->
+    <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+      <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+        <div><span style="font-size: 12px;">학생복지처</span></div>
+        <div  style="margin-top: -5px;"></div>
+      </div>
+      <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+        <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+        </div>
+        <div style="float: right;line-height: 40px;">
+          <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+        </div>
+      </div>
+    </div>
+
+</div>
+<!-- second col-->
+<div class="col-xs-6" style="padding:0 5px;float:left;">
+  <!-- 국제처-->
+  <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+    <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+      <div><span style="font-size: 12px;">국제처</span></div>
+      <div  style="margin-top: -5px;"></div>
+    </div>
+    <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+      <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+      </div>
+      <div style="float: right;line-height: 40px;">
+        <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+      </div>
+    </div>
+  </div>
+  <!--  대학교회 -->
+  <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+    <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+      <div><span style="font-size: 12px;">대학교회</span></div>
+      <div  style="margin-top: -5px;"></div>
+    </div>
+    <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+      <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+      </div>
+      <div style="float: right;line-height: 40px;">
+        <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+      </div>
+    </div>
+  </div>
+  <!-- 생활관-->
+  <div class="card" style="height:50px; margin-bottom:6px; position:relative;">
+    <div style="height: 100%;width: 4em; border-right:2px solid #fcd90d;">
+      <div><span style="font-size: 12px;">생활관</span></div>
+      <div  style="margin-top: -5px;"></div>
+    </div>
+    <div style="position: absolute;top: 4px;left: 0;width: 100%;height: 42px;padding-left: 4em;padding-right: 18px;">
+      <div style="position: absolute;bottom: 2px;font-size: 10px;padding-left: 18px;">
+      </div>
+      <div style="float: right;line-height: 40px;">
+        <span> <a href="tel:01058521555"><img class="img img-responsive" src="/src/img/4link.png"></a></span>
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
 </div>
