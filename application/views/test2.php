bd41094d59f72ade44c98ca6edd1a738a99603d2
Woohyun Kim <r45r54r45@naver.com>
2016-03-02 00:51:13 +0900

    ddd

--- application/views/test2.php
+++ application/views/test2.php
@@ -1,48 +1,49 @@
 <?
-$content= file_get_contents("http://bus.incheon.go.kr/iw/pda/01/retrievePdaOfferBusSystem2892273231.laf?routeid=165000381&bstopid=165000725&pathseq=30");
-$time=strrpos($content,'txt = txt + "');
-$pos= stripos($content,'txt = txt + "');
+
+$content= file_get_contents("http://bus.incheon.go.kr/iwcm/retrievebusstopcararriveinfo.laf?bstopid=165000725&routeid=165000381&pathseq=30");
+$time=strrpos($content,'<font color="red">');
+$pos= stripos($content,'<font color="red">');
 $b6724=array("","");
 if(!$pos||!$time){
 
 }
 else{
-$b6724[0]=substr($content,$pos+13,1);
-$b6724[1]=substr($content,$time+13,2);
+$b6724[0]=substr($content,$pos+18,1);
+$b6724[1]=substr($content,$time+18,2);
 }
 //end of 6724
 
-$content= file_get_contents("http://bus.incheon.go.kr/iw/pda/01/retrievePdaOfferBusSystem2892273231.laf?routeid=165000245&bstopid=165000861&pathseq=33");
-$time=strrpos($content,'txt = txt + "');
-$pos= stripos($content,'txt = txt + "');
+$content= file_get_contents("http://bus.incheon.go.kr/iwcm/retrievebusstopcararriveinfo.laf?bstopid=165000861&routeid=165000245&pathseq=33");
+$time=strrpos($content,'<font color="red">');
+$pos= stripos($content,'<font color="red">');
 $b9201=array("","");
 if(!$pos||!$time){}
 else{
-$b9201[0]=substr($content,$pos+13,1);
-$b9201[1]=substr($content,$time+13,2);
+$b9201[0]=substr($content,$pos+18,1);
+$b9201[1]=substr($content,$time+18,2);
 }
 //end of 9201
 
 
-$content= file_get_contents("http://bus.incheon.go.kr/iw/pda/01/retrievePdaOfferBusSystem2892273231.laf?routeid=165000206&bstopid=165000848&pathseq=74&routedir=1");
-$time=strrpos($content,'txt = txt + "');
-$pos= stripos($content,'txt = txt + "');
+$content= file_get_contents("http://bus.incheon.go.kr/iwcm/retrievebusstopcararriveinfo.laf?bstopid=165000848&routeid=165000206&pathseq=74");
+$time=strrpos($content,'<font color="red">');
+$pos= stripos($content,'<font color="red">');
 $b91=array("","");
 if(!$pos||!$time){}
 else{
-$b91[0]=substr($content,$pos+13,1);
-$b91[1]=substr($content,$time+13,2);
+$b91[0]=substr($content,$pos+18,1);
+$b91[1]=substr($content,$time+18,2);
 }
 //end of 91
 
-$content= file_get_contents("http://bus.incheon.go.kr/iw/pda/01/retrievePdaOfferBusSystem2892273231.laf?routeid=165000215&bstopid=164000016&pathseq=41&routedir=0");
-$time=strrpos($content,'txt = txt + "');
-$pos= stripos($content,'txt = txt + "');
+$content= file_get_contents("http://bus.incheon.go.kr/iwcm/retrievebusstopcararriveinfo.laf?bstopid=164000016&routeid=165000215&pathseq=41");
+$time=strrpos($content,'<font color="red">');
+$pos= stripos($content,'<font color="red">');
 $b6405=array("","");
 if(!$pos||!$time){}
 else{
-$b6405[0]=substr($content,$pos+13,1);
-$b6405[1]=substr($content,$time+13,2);
+$b6405[0]=substr($content,$pos+18,1);
+$b6405[1]=substr($content,$time+18,2);
 }
 //end of 6405
 
