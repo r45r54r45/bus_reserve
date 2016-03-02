ee0c23ec17f5381a439915f668689cf49471bb09
Woohyun Kim <r45r54r45@naver.com>
2016-03-02 09:43:46 +0900

    ddd

--- application/views/new_main.php
+++ application/views/new_main.php
@@ -55,15 +55,6 @@
   <script src="/src/js/bootstrap.min.js"></script>
   <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
   <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-  <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-73228564-1', 'auto');
-  ga('send', 'pageview');
-
-  </script>
 </body>
 </html>
