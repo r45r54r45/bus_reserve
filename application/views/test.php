59d44351a23a39c45bc949618ea9bf6828633fb5
Woohyun Kim <r45r54r45@naver.com>
2016-03-02 12:33:15 +0900

    ddd

--- application/views/test.php
+++ application/views/test.php
@@ -1,37 +1,59 @@
 <?
-header('Access-Control-Allow-Origin:*');
-header('Content-Type: application/json');
+$result = file_get_contents('http://ysweb.yonsei.ac.kr/ysbus.jsp');
+$JSESSIONID= substr($http_response_header[8],23,91);
+$__smVisitorID= substr($http_response_header[7],26,11);
+$postdata = http_build_query(
+array(
+  'userid' => '2014198024',
+  'password' => '1852512'
+)
+);
+$opts = array(
+  "http"=>array(
+    "method"=>"POST",
+    "header"=>"Content-type: application/x-www-form-urlencoded\r\n".
+    "Cookie: JSESSIONID=$JSESSIONID; __smVisitorID=$__smVisitorID",
+    "content" => $postdata
+  )
+);
+$context = stream_context_create($opts);
+$file = file_get_contents('http://ysweb.yonsei.ac.kr/ysbus_main.jsp', false, $context);
+$opts = array(
+  "http"=>array(
+    "method"=>"GET",
+    "header"=>"Content-type: application/x-www-form-urlencoded\r\n".
+    "Cookie: JSESSIONID=$JSESSIONID; __smVisitorID=$__smVisitorID"
+  )
+);
+$context = stream_context_create($opts);
+//api selector
 
-$url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?ServiceKey=JwH8P3QCOaTv%2FYttZkCDASHiJZblEJAk8X2i6nICtptOT8O55JvqRPG7nluVp6QEVQu7sy51aHk%2FUgm2b0Ncyg%3D%3D&cityCode=23&routeId=ICB165000381&nodeId=ICB165000725"; //6724
-$response = file_get_contents($url);
-$object = simplexml_load_string($response);
-$count6724=$object->body->items->item->arrprevstationcnt;
-$time6724= $object->body->items->item->arrtime;
 
-$url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?ServiceKey=JwH8P3QCOaTv%2FYttZkCDASHiJZblEJAk8X2i6nICtptOT8O55JvqRPG7nluVp6QEVQu7sy51aHk%2FUgm2b0Ncyg%3D%3D&cityCode=23&routeId=ICB165000381&nodeId=ICB165000725"; //6724
-$response = file_get_contents($url);
-$object = simplexml_load_string($response);
-$json = json_encode($object);
-$count6405=$object->body->items->item->arrprevstationcnt;
-$time6405= $object->body->items->item->arrtime;
+$file = file_get_contents('http://ysweb.yonsei.ac.kr/busTest/reserveinfo2.jsp', false, $context);
+$DOM = new DOMDocument;
+$DOM->loadHTML($file);
+$tbody = $DOM->getElementsByTagName('tbody');
+$tr=$tbody->item(0)->getElementsByTagName('tr');
+$result=array();
+for ($i=0; $i < $tr->length; $i++) {
+  $td=$tr->item($i)->getElementsByTagName('td');
+  $departure= $td->item(0)->nodeValue;
+  $time=$td->item(1)->nodeValue;
+  $seatNum=$td->item(2)->nodeValue;
+  //출발위치
+  $departure=explode('(',$departure);
+  //시간
+  $time=explode(' ',$time);
+  $temp_date=explode('/',$time[0]);
+  $time=$time[1];
+  $date=array("year"=>$temp_date[0],"month"=>$temp_date[1],"date"=>$temp_date[2]);
+  //자리
+  $pos=strrpos($seatNum,"Seat No:");
+  $seatNum= substr($seatNum,$pos+9,strlen($seatNum)-$pos-10);
 
-$url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?ServiceKey=JwH8P3QCOaTv%2FYttZkCDASHiJZblEJAk8X2i6nICtptOT8O55JvqRPG7nluVp6QEVQu7sy51aHk%2FUgm2b0Ncyg%3D%3D&cityCode=23&routeId=ICB165000381&nodeId=ICB165000725"; //91
-$response = file_get_contents($url);
-$object = simplexml_load_string($response);
-$json = json_encode($object);
-$count91=$object->body->items->item->arrprevstationcnt;
-$time91= $object->body->items->item->arrtime;
-
-$url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList?ServiceKey=JwH8P3QCOaTv%2FYttZkCDASHiJZblEJAk8X2i6nICtptOT8O55JvqRPG7nluVp6QEVQu7sy51aHk%2FUgm2b0Ncyg%3D%3D&cityCode=23&routeId=ICB165000381&nodeId=ICB165000725"; //9201
-$response = file_get_contents($url);
-$object = simplexml_load_string($response);
-$json = json_encode($object);
-$count9201=$object->body->items->item->arrprevstationcnt;
-$time9201= $object->body->items->item->arrtime;
-
-echo '{
-  "6724":{"count":'.$count6724.',"time":'.$time6724.'},
-  "6405":{"count":'.$count6405.',"time":'.$time6405.'},
-  "91":{"count":'.$count91.',"time":'.$time91.'},
-  "9201":{"count":'.$count9201.',"time":'.$time9201.'}
-}';
+  $result[$i]["departure"]=($departure[0]=="신촌캠퍼스 "?"C":"D");
+  $result[$i]["time"]["date"]=$date;
+  $result[$i]["time"]["time"]=$time;
+  $result[$i]["seatNum"]=$seatNum;
+}
+echo json_encode($result); //예약 내역
