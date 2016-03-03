0ddaae9bf56ef6d14502e2b87015e68b26956912
Jaeha Lee <jhl.webproduction@gmail.com>
2016-03-04 01:14:04 +0900

    asdf

--- application/views/new_main.php
+++ application/views/new_main.php
@@ -42,7 +42,7 @@
 
   </iframe>
 
-  <div class="navbar-fixed-bottom" style="padding:0;">
+  <div data-role="footer" class="container-fluid" style="padding:0;" id="nav-bottom">
     <ul class="nav nav-pills">
       <li id="menu1" class="center-align nav-bottom-border nav-bottom-5">
         <img class="img img-responsive" src="/src/img/1home_o.png"/>
