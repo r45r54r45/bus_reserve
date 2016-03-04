f7850e7b2a7cc22c1c2dc5f644a33d1b6d36f735
Jaeha Lee <jhl.webproduction@gmail.com>
2016-03-04 20:37:44 +0900

    pdf

--- application/views/new_main.php
+++ application/views/new_main.php
@@ -37,11 +37,10 @@
     <div class="back_arrow"><img src="/src/img/backbtn.png" style="    height: 18px;"></div>
     <span class="nav-font-color nav-font-valign" id="page_title" style="font-family:'Montserrat'; color:white;text-shadow: 0 0px 0 #eee !important; font-weight:100"></span>
   </div>
-  <div style=" overflow:auto; position:absolute; -webkit-overflow-scrolling: touch; width:100%; height:100%;">
-  <iframe id="contents" frameBorder="0" style="height:2000px;" scrolling="no">
+
+  <iframe id="contents" frameBorder="0">
 
   </iframe>
-  </div>
 
   <div data-role="footer" class="container-fluid" style="padding:0;" id="nav-bottom">
     <ul class="nav nav-pills">
