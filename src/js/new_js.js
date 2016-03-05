cc247c32b1ff9474fdba4a8281d789d551bcf624
Woohyun Kim <r45r54r45@naver.com>
2016-03-06 00:01:25 +0900

    ddd

--- src/js/new_js.js
+++ src/js/new_js.js
@@ -50,7 +50,8 @@ $(function(){
     $("#menu3>img").attr("src","/src/img/3menu.png");
     $("#menu4>img").attr("src","/src/img/4link.png");
     location.replace("#more");
-      document.getElementById("makelong").style.height = "100%";
+    $("html").scrollTop(0);
+    document.getElementById("makelong").style.height = "100%";
     // document.getElementById("makelong").style.height="0px";
     // document.getElementById("makelong").style.height = "2200px";
     content_manager();
