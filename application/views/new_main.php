a349bd7fc6dc9a5f8920e28ba059a24716ad2f3f
Woohyun Kim <r45r54r45@naver.com>
2016-03-06 11:52:47 +0900

    iframe fix

--- application/views/new_main.php
+++ application/views/new_main.php
@@ -38,14 +38,13 @@
     <span class="nav-font-color nav-font-valign" id="page_title" style="font-family:'Montserrat'; color:white;text-shadow: 0 0px 0 #eee !important; font-weight:100"></span>
   </div>
 
-<div id="makelong" style="background-color:#ffffff"></div>
+<div id="makelong" style="background-color:#ffffff; overflow-y:auto; ">
 
 
 
   <iframe id="contents" frameBorder="0">
 
   </iframe>
-  <div>
 
 </div>
 
