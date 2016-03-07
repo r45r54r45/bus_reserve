40131fb69375586beb2498a924f40e485ae97371
Woohyun Kim <r45r54r45@naver.com>
2016-03-07 20:43:12 +0900

    ㅇㅇ

--- application/views/test3.php
+++ application/views/test3.php
@@ -1,3 +1,3 @@
 <?
-$result=file_get_contents("/api/login?id=2014198024&pw=1852512");
+$result=file_get_contents("http://ybanana.yonsei.ac.kr/api/login?id=2014198024&pw=1852512");
 var_dump(json_decode($result));
