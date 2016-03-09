79a589a552dd86f3b75e46b087b10b90e4d7ca98
Woohyun Kim <r45r54r45@naver.com>
2016-03-09 22:37:05 +0900

    d

--- application/views/shuttle_login.php
+++ application/views/shuttle_login.php
@@ -1,4 +1,4 @@
-<form name="frmLogin" id="form" action="http://ysweb.yonsei.ac.kr/ysbus_main.jsp" method="post">
+<form name="frmLogin" id="form" style="display:none;"  action="http://ysweb.yonsei.ac.kr/ysbus_main.jsp" method="post">
   <input type="text" id="id" name="userid" value="<?echo $_GET['id']?>">
   <input type="password" id="password" name="password"  value="<?echo $_GET['pw']?>">
 </form>
