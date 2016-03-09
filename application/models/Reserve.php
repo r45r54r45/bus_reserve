02ed6c5311fb8390f8466579d45408603741d509
Woohyun Kim <r45r54r45@naver.com>
2016-03-09 16:41:22 +0900

    ddd

--- application/models/Reserve.php
+++ application/models/Reserve.php
@@ -12,7 +12,8 @@ class Reserve extends CI_Model{
 	return $this->db->query("insert into r_reserve (r_user,	r_date,	r_week,	r_time,	r_loc) values ('$id', '$date', '$week', '$time', '$loc')");
 	}
 	public function getByDate($r_date){
-		return $this->db->query("select id, pw, r_reserve_id, r_loc, r_time from r_reserve, r_user where r_user=id and r_date='$r_date'")->result();
+		$r= $this->db->query("select id, pw, r_reserve_id, r_loc, r_time from r_reserve, r_user where r_user=id and r_date='$r_date'");
+		return $r->result();
 	}
 	public function getByWeek($r_week){
 		return $this->db->query("select id, pw, r_reserve_id,  r_loc, r_time from r_reserve, r_user where r_user=id and r_week='$r_week'")->result();
