7b208c35cad54d9563e21a7cfaf5793193da90d9
Woohyun Kim <r45r54r45@naver.com>
2016-03-13 14:44:09 +0900

    ddd

--- src/js/new_js.js
+++ src/js/new_js.js
@@ -67,7 +67,7 @@ $(function(){
   $(".noti").on("click",function(){
     if(noti_flag==true){
     $("#noti_modal").modal({
-      backdrop: false
+      backdrop: true
     }).modal('show');
   }
   });
