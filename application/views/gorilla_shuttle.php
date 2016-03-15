6bd519f4afd02d2508e54b3aa202bddc1496b91a
Woohyun Kim <r45r54r45@naver.com>
2016-03-15 15:55:30 +0900

    ddd

--- application/views/gorilla_shuttle.php
+++ application/views/gorilla_shuttle.php
@@ -226,4 +226,17 @@ function reserve(time, day, loc, type){
   });
 
 }
+function getCookie(cName) {
+    cName = cName + '=';
+    var cookieData = document.cookie;
+    var start = cookieData.indexOf(cName);
+    var cValue = '';
+    if(start != -1){
+        start += cName.length;
+        var end = cookieData.indexOf(';', start);
+        if(end == -1)end = cookieData.length;
+        cValue = cookieData.substring(start, end);
+    }
+    return unescape(cValue);
+}
 </script>
