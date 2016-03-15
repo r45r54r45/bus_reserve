316dd101f31bc0d56361612941f2ad151a277403
Woohyun Kim <r45r54r45@naver.com>
2016-03-15 16:44:29 +0900

    fff

--- application/views/gorilla_shuttle.php
+++ application/views/gorilla_shuttle.php
@@ -188,7 +188,7 @@ $(function(){
 function cancel(cellId){
   var user=getCookie("final_userIdx");
   $("#"+cellId).text("").css("background","none");
-  $.get("/data/deleteReserve/"+user+"/"+selectedCell,function(data){
+  $.get("/data/deleteReserve/"+user+"/"+cellId,function(data){
     //파라미터로 제거할 시간 요일을 보낸다.
   });
 }
