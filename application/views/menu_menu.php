e8a0f78831a8728490daff0a141107319341317e
Woohyun Kim <r45r54r45@naver.com>
2016-03-21 09:34:48 +0900

    ddd

--- application/views/menu_menu.php
+++ application/views/menu_menu.php
@@ -102,7 +102,7 @@ padding-right: 18px;">
     <h4 class="modal-title" id="myModalLabel">전체 시간표</h4>
   </div> -->
   <div class="modal-body" style="padding:0">
-    <img  style="    width: 100%;" src="/src/img/yplaza314.png" onclick="$('#myModal').modal('hide')">
+    <img  style="    width: 100%;" src="/src/img/yplaza321.png" onclick="$('#myModal').modal('hide')">
   </div>
   <!-- <div class="modal-footer">
     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
