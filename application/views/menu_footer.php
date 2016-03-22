1b1891c34b837cb72df76daee9244e7fdc0683f1
Woohyun Kim <r45r54r45@naver.com>
2016-03-23 01:26:37 +0900

    nn

--- application/views/menu_footer.php
+++ application/views/menu_footer.php
@@ -3,10 +3,10 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
+ga('set', 'userId', getCookie("id"));
 ga('create', 'UA-73228564-1', 'auto');
 ga('send', 'pageview');
-ga('set', 'userId', getCookie("id"));
+
 
 function getCookie(cName) {
     cName = cName + '=';
