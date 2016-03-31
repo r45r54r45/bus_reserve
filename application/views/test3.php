752bead67901313bcc62d90d6109f8b0e159b7c0
Woohyun Kim <r45r54r45@naver.com>
2016-03-31 15:43:12 +0900

    ddd

--- application/views/test3.php
+++ application/views/test3.php
@@ -125,4 +125,17 @@ $(function(){
     alert("준비중입니다!");
   });
 });
+function getCookie(cName) {
+  cName = cName + '=';
+  var cookieData = document.cookie;
+  var start = cookieData.indexOf(cName);
+  var cValue = '';
+  if(start != -1){
+    start += cName.length;
+    var end = cookieData.indexOf(';', start);
+    if(end == -1)end = cookieData.length;
+    cValue = cookieData.substring(start, end);
+  }
+  return unescape(cValue);
+}
 </script>
