8bb6fdb6a7053aef16aa773770f3e47d16461762
Woohyun Kim <r45r54r45@naver.com>
2016-04-10 13:38:37 +0900

    ddd

--- application/views/more_delivery.php
+++ application/views/more_delivery.php
@@ -52,19 +52,19 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="/src/img/splash2.png" alt="Chania">
+      <img src="/src/img/yonsei.png" alt="Chania">
     </div>
 
     <div class="item">
-      <img src="/src/img/splash2.png" alt="Chania">
+      <img src="/src/img/yonsei.png" alt="Chania">
     </div>
 
     <div class="item">
-      <img src="/src/img/splash2.png" alt="Flower">
+      <img src="/src/img/yonsei.png" alt="Flower">
     </div>
 
     <div class="item">
-      <img src="/src/img/splash2.png" alt="Flower">
+      <img src="/src/img/yonsei.png" alt="Flower">
     </div>
   </div>
 
