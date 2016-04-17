057c656b0fda0e7b95ce750795e4600400bb0e7d
Woohyun Kim <r45r54r45@naver.com>
2016-04-18 00:28:24 +0900

    ㅗ

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 "use strict"
 var app=angular.module("app",["firebase"]);
 app.factory("Ref",
-  function() {
-    var ref = new Firebase("https://sizzling-inferno-3457.firebaseio.com");
-    return ref;
-  }).filter('toArray', function () {
+function() {
+  var ref = new Firebase("https://sizzling-inferno-3457.firebaseio.com");
+  return ref;
+}).filter('toArray', function () {
   return function (obj, addKey) {
     if (!(obj instanceof Object)) {
       return obj;
@@ -27,43 +27,69 @@ app
   var target="";
   switch (location.search.split('target=')[1]) {
     case "chicken":
-      target="치킨";
-      //아직 계약해야할 업체들
-      $scope.moreStores=[
-        {"store":"멕시카나","menu_img":"",
+    target="치킨";
+    //아직 계약해야할 업체들
+    $scope.moreStores=[
+      {"store":"멕시카나","menu_img":"",
       "tel":"0328319266"},
-        {"store":"강호동치킨","menu_img":"",
+      {"store":"강호동치킨","menu_img":"",
       "tel":"0328146780"},
       {"store":"호식이두마리치킨","menu_img":"",
-    "tel":"0328582332"},
-        {"store":"교촌치킨","menu_img":"/src/img/KCmenu.jpeg",
+      "tel":"0328582332"},
+      {"store":"교촌치킨","menu_img":"/src/img/KCmenu.jpeg",
       "tel":"0328580060"},
-        {"store":"깻잎치킨","menu_img":"",
+      {"store":"깻잎치킨","menu_img":"",
       "tel":"0328345466"},
-        {"store":"BBQ치킨","menu_img":"/src/img/BBQmenu.png",
+      {"store":"BBQ치킨","menu_img":"/src/img/BBQmenu.png",
       "tel":"0328589200"},
-        {"store":"맛있는 파닭","menu_img":"",
+      {"store":"맛있는 파닭","menu_img":"",
       "tel":"0328357441"},
-        {"store":"디디치킨","menu_img":"/src/img/DDChicken.png",
+      {"store":"디디치킨","menu_img":"/src/img/DDChicken.png",
       "tel":"0328588105"},
       {"store":"굽네치킨","menu_img":"",
-    "tel":"0328338294"}
-      ];
+      "tel":"0328338294"}
+    ];
     break;
     case "fastfood":
-      target="패스트푸드";
+    target="패스트푸드";
+    $scope.moreStores=[
+      {"store":"버거킹","menu_img":"",
+      "tel":"15990505"},
+      {"store":"롯데리아","menu_img":"",
+      "tel":"0328581777"}
+    ];
     break;
     case "jokbal":
-      target="족발";
+    target="족발";
+    $scope.moreStores=[
+      {"store":"원조압구정보쌈족발","menu_img":"",
+      "tel":"0328125858"},
+      {"store":"원할머니보쌈","menu_img":"",
+      "tel":"0328580030"},
+      {"store":"장충동보쌈","menu_img":"",
+      "tel":"0328347521"},
+      {"store":"할매보쌈","menu_img":"",
+      "tel":"0328130003"}
+    ];
     break;
     case "pizza":
-      target="피자";
+    target="피자";
+    $scope.moreStores=[
+      {"store":"원조압구정보쌈족발","menu_img":"",
+      "tel":"0328125858"},
+      {"store":"원할머니보쌈","menu_img":"",
+      "tel":"0328580030"},
+      {"store":"장충동보쌈","menu_img":"",
+      "tel":"0328347521"},
+      {"store":"할매보쌈","menu_img":"",
+      "tel":"0328130003"}
+    ];
     break;
     case "chinese":
-      target="중국집";
+    target="중국집";
     break;
     case "korean":
-      target="한식";
+    target="한식";
     break;
   }
   $scope.stores=$firebaseArray(Ref.child("store").orderByChild("type").equalTo(target));
