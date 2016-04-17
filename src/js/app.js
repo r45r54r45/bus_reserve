704a44e5fab912f52da39786aafa6cbe84e346cf
Woohyun Kim <r45r54r45@naver.com>
2016-04-17 17:28:40 +0900

    ddd

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 var app=angular.module("app",[]);
 app.controller("deliveryMain",function($scope){
   var BA=new BannerAdmin();
-  $scope.bannerUrl=BA.selectedBannerUrl();
+  // $scope.bannerUrl=BA.selectedBannerUrl();
 });
