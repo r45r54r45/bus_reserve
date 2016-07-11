8a19932c83f59e42497bf3e0ad1bff9a05312301
Jaeha Lee <jhl.webproduction@gmail.com>
2016-07-11 19:43:50 +0900

    bb

--- application/views/new_main.php
+++ application/views/new_main.php
@@ -1,5 +1,29 @@
 
-<div style="background: #fcd90d;
-    padding-top: 30%; color: #46292b; text-align:center; width:100vw; width:100%; height:100vh; height:100%;">
-<img src="../src/img/main_logo.png" style="width:50%;"><br>
-  바나나가 9월 1일에 새롭게 태어납니다. ㅂㅂ </div>
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title></title>
+  </head>
+  <body style="margin:0px;">
+    <div style="background: #fcd90d;
+    padding-top: 60%;
+    color: #46292b;
+    text-align: center;
+    width: 100%;
+    width: 100vw;
+    height: 100%;
+    height: 100vh;
+    font-size: 12px;
+    font-family: sans-serif;">
+    <img src="../src/img/main_logo.png" style="    width: 30%;
+    margin-bottom: 10px;"><br>
+      <span style="">바나나가 9월 1일에 새롭게 태어납니다. ㅂㅂ</span> </div>
+  </body>
+</html>
